e20698230bdab72322ddd85576bd4e70b0cf10a0
Andy <andy.doan@bholdus.com>
2021-11-26 12:51:40 +0800

    Update prod build for explorer

--- packages/next/run.sh
+++ packages/next/run.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-pm2-runtime start ecosystem.config.js --only statescan-next --watch --env development
+# pm2-runtime start ecosystem.config.js --only statescan-next --watch --env development
+pm2-runtime start ecosystem.config.js --only statescan-next-prod --env production
 # npm run scan
